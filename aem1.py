bd8424e83df52347b2cdc23685792954493a78e8
Adam Stelmaszyk <adam.stelmaszyk@pointpack.pl>
2021-03-19 09:45:09 +0100

    add

--- aem1.py
+++ aem1.py
@@ -1,12 +1,45 @@
+import numpy as np
+import math
 
 
-input_file = []
-data = []
+nodes = []
+
+
+def distance(pt1, pt2):
+    return math.sqrt((pt1[1] - pt2[1])**2 + (pt1[2] - pt2[2])**2)
+
+
+def convert_to_int_table(str_nodes):
+    return [int(str_nodes[0]), int(str_nodes[1]), int(str_nodes[2])]
+
+
+def alg1(init_node):
+    # print(init_node)
+    visited_nodes = []
+    path_length = 0
+    iterator = 50
+    node = nodes[init_node]
+    nodes.remove(node)
+
+    
+
+    while iterator > 0:
+        
+        # for nighbour
+        
+        # iterator = iterator - 1
+
+    return
+
+
 with open("kroA100.txt", 'r') as instance:
-    infile_tasks = instance.readlines()
-    infile_tasks = [line[:].replace('\n',"").split(' ') for line in infile_tasks][1:]
+    str_nodes = instance.readlines()
+    nodes = [convert_to_int_table(line[:].replace(
+        '\n', "").split(' ')) for line in str_nodes]
 
 
+alg1(np.random.randint(0, 100))
 
+# print(str(distance([1, 1380, 939], [2, 2848, 96])))
 
-print(infile_tasks)
+# print(nodes)
