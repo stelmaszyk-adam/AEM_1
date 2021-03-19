5d31b65697c8cbbca8288c7f4d958ed6e395254f
Adam Stelmaszyk <adam.stelmaszyk@pointpack.pl>
2021-03-19 08:59:57 +0100

    add

--- aem1.py
+++ aem1.py
@@ -4,7 +4,9 @@ input_file = []
 data = []
 with open("kroA100.txt", 'r') as instance:
     infile_tasks = instance.readlines()
-    infile_tasks = [line[:].split(' ') for line in infile_tasks][1:]
+    infile_tasks = [line[:].replace('\n',"").split(' ') for line in infile_tasks][1:]
+
+
 
 
 print(infile_tasks)
